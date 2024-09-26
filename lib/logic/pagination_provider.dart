c90ef1669d4b7e7ff3db8882965dfc6b74c340bb
mohamad Elgamal <mohamadelgamal2002.9.1@gmail.com>
2024-09-26 13:21:54 +0300

    added search feature with pagination

--- lib/logic/pagination_provider.dart
+++ lib/logic/pagination_provider.dart
@@ -1,68 +1,109 @@
-import 'dart:convert';
-import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'package:logger/logger.dart';
-import '../model/model_class.dart';
+import 'dart:convert'; // Importing the Dart library for JSON encoding and decoding
+import 'package:flutter/material.dart'; // Importing Flutter's material design library
+import 'package:http/http.dart' as http; // Importing the HTTP package for making network requests
+import 'package:pagination_project/model/category_response_model.dart'; // Importing the model for category response
 
 class PaginationProvider with ChangeNotifier {
-  /// List to store the fetched results.
-  List<Result> result = [];
-
-  /// Boolean to indicate if the initial data is being loaded.
+  // List to store the fetched results
+  List<CategoryDetails> result = [];
+  // List to store the search results
+  List<CategoryDetails> resultSearch = [];
+  // Flags to manage loading states
   bool loading = true;
-
-  /// Boolean to indicate if more data is being loaded when the user scrolls.
   bool loadingMore = false;
-
-  /// Integer to keep track of the current offset for pagination.
+  bool hasMoreData = true;
+  // Offset for pagination
   int offset = 1;
-
-  /// ScrollController to manage the scroll position and detect when the user has scrolled to the bottom.
+  // Scroll and search controllers
   final ScrollController scrollController = ScrollController();
+  final TextEditingController searchController = TextEditingController();
+  // Flag to check if searching
+  bool _isSearching = false;
 
-  /// Constant string for the API URL.
-  static const String apiUrl = "https://pokeapi.co/api/v2/pokemon";
-  static const String appKey = "base64:7+4+S0ntVJXJgJWh8A1axdkJZuNRGfJZOu2pDL1zloA=";
+  // API URLs and app key
+  static const String apiUrl = "Example URL";
+  static const String searchUrl = "Example URL";
+  static const String appKey = "Example Key";
 
+  // Constructor to initialize the provider
   PaginationProvider() {
     _initialize();
+    // searchController.addListener(_onSearchChanged); // Uncomment if you want to listen to search input changes
   }
 
-  /// Initializes the provider by fetching initial data and setting up the scroll listener.
+  // Initialize the provider by fetching data and setting up scroll listener
   void _initialize() {
     fetchData(offset);
     _setupScrollListener();
   }
 
-  /// Fetches data from the API based on the given offset.
-  /// Updates the loading state and handles the response.
+  // Listener for search input changes
+  void _onSearchChanged() {
+    search(searchController.text);
+  }
+
+  // Fetch data from the API
   Future<void> fetchData(int paraOffset) async {
+    if (!hasMoreData) return;
+
     _setLoadingState(paraOffset);
 
     try {
-      final response = await http.get(Uri.parse("$apiUrl?offset=$paraOffset&limit=15"));
-
-      print("print : ${paraOffset}");
-      print("Response: ${response.body}");
+      final response = await http.get(
+        Uri.parse("$apiUrl?page=$paraOffset"),
+        headers: {
+          'APP_KEY': '$appKey',
+          'Accept': 'application/json',
+        },
+      );
       if (response.statusCode == 200) {
-        final modelClass = ModelClass.fromJson(json.decode(response.body));
-        _updateResults(modelClass.results, paraOffset);
-
+        final data = json.decode(response.body)['data'];
+        if (data.isNotEmpty) {
+          final modelClass = CategoryDetailsModel.fromJson(json.decode(response.body));
+          _updateResults(modelClass.data, paraOffset);
+        } else {
+          hasMoreData = false;
+        }
       } else {
         throw Exception('Failed to load data');
       }
     } catch (e) {
-      // Handle error
       print("Error fetching data: $e");
     } finally {
       _resetLoadingState();
     }
   }
 
-  /// Sets the loading state based on the offset.
-  /// If offset is 0, it indicates initial loading, otherwise it's loading more data.
+  // Search data from the API
+  Future<void> search(String keyword) async {
+    _setLoadingState(0);
+
+    try {
+      final response = await http.post(
+        Uri.parse("$searchUrl?page=1"),
+        headers: {
+          'APP_KEY': '$appKey',
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: json.encode({'keyword': keyword}),
+      );
+      if (response.statusCode == 200) {
+        final modelClass = CategoryDetailsModel.fromJson(json.decode(response.body));
+        _updateResultsSearch(modelClass.data ?? [], 1);
+      } else {
+        throw Exception('Failed to load data');
+      }
+    } catch (e) {
+      print("Error searching data: $e");
+    } finally {
+      _resetLoadingState();
+    }
+  }
+
+  // Set loading state based on the offset
   void _setLoadingState(int paraOffset) {
-    if (paraOffset == 1) {
+    if (paraOffset == 0) {
       loading = true;
     } else {
       loadingMore = true;
@@ -70,16 +111,15 @@ class PaginationProvider with ChangeNotifier {
     notifyListeners();
   }
 
-  /// Resets the loading state after data is fetched.
+  // Reset loading state
   void _resetLoadingState() {
     loading = false;
     loadingMore = false;
     notifyListeners();
   }
 
-  /// Updates the results list with new data.
-  /// If offset is 0, it replaces the list, otherwise it appends to the existing list.
-  void _updateResults(List<Result> newResults, int paraOffset) {
+  // Update the results list with new data
+  void _updateResults(List<CategoryDetails> newResults, int paraOffset) {
     if (paraOffset == 1) {
       result = newResults;
     } else {
@@ -89,8 +129,18 @@ class PaginationProvider with ChangeNotifier {
     notifyListeners();
   }
 
-  /// Sets up a scroll listener to detect when the user has scrolled to the bottom of the list.
-  /// Fetches more data when the bottom is reached.
+  // Update the search results list with new data
+  void _updateResultsSearch(List<CategoryDetails> newResults, int paraOffset) {
+    if (paraOffset == 1) {
+      resultSearch = newResults;
+    } else {
+      resultSearch.addAll(newResults);
+    }
+    offset = paraOffset + 1;
+    notifyListeners();
+  }
+
+  // Setup scroll listener to fetch more data when reaching the end of the list
   void _setupScrollListener() {
     scrollController.addListener(() {
       if (scrollController.position.maxScrollExtent == scrollController.position.pixels) {
@@ -99,14 +149,26 @@ class PaginationProvider with ChangeNotifier {
     });
   }
 
-  /// Refreshes the data by fetching it from the beginning (offset 0).
+  // Refresh the data
   Future<void> refresh() async {
+    hasMoreData = true;
     await fetchData(1);
   }
 
-  /// Disposes the scroll controller when the provider is disposed.
+  // Toggle the search state
+  void toggleSearch() {
+    _isSearching = !_isSearching;
+    notifyListeners();
+  }
+
+  // Getter for the searching state
+  bool get isSearching => _isSearching;
+
+  // Dispose controllers and listeners
   @override
   void dispose() {
+    searchController.removeListener(_onSearchChanged);
+    searchController.dispose();
     scrollController.dispose();
     super.dispose();
   }
--- lib/ui/pagination_screen.dart
+++ lib/ui/pagination_screen.dart
@@ -1,51 +1,120 @@
 import 'package:flutter/material.dart';
 import 'package:pagination_project/ui/widget/dynamic_card.dart';
 import 'package:pagination_project/ui/widget/dynamic_list_view.dart';
-import 'package:pagination_project/ui/widget/dynamic_sliver_list_view.dart';
 import 'package:pagination_project/ui/widget/shimmer_row.dart';
 import 'package:provider/provider.dart';
 
 import '../logic/pagination_provider.dart';
-import '../model/model_class.dart';
+import '../model/category_response_model.dart';
 
 /// A stateless widget that represents the pagination screen.
 class PaginationScreen extends StatelessWidget {
-  /// Creates a [PaginationScreen] widget.
+  /// Constructor for the PaginationScreen widget.
   const PaginationScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
+      /// Creates an instance of PaginationProvider and provides it to the widget tree.
       create: (_) => PaginationProvider(),
       child: Scaffold(
+        /// The main structure of the screen.
         appBar: AppBar(
-          title: const Text(
-            "Scroll Pagination",
-            style: TextStyle(color: Colors.white),
+          /// The title of the app bar, which changes based on the search state.
+          title: Consumer<PaginationProvider>(
+            /// Builds the title widget based on the provider's state.
+            builder: (context, provider, child) {
+              return AnimatedSwitcher(
+                /// Animation duration for switching between title and search field.
+                duration: const Duration(milliseconds: 300),
+                child: provider.isSearching
+                    ? TextField(
+                  /// Key for the search field.
+                  key: ValueKey('searchField'),
+                  /// Controller for the search input.
+                  controller: provider.searchController,
+                  /// Decoration for the search input field.
+                  decoration: const InputDecoration(
+                    hintText: 'Search...',
+                    border: InputBorder.none,
+                    hintStyle: TextStyle(color: Colors.white),
+                  ),
+                  /// Style for the search input text.
+                  style: const TextStyle(color: Colors.white),
+                  /// Callback for when the search input changes.
+                  onChanged: (value) {
+                    provider.search(value);
+                  },
+                  /// Callback for when the search input is submitted.
+                  onSubmitted: (value) {
+                    // Do nothing on submit
+                  },
+                )
+                    : const Text(
+                  /// Title text when not searching.
+                  "Scroll Pagination",
+                  key: ValueKey('titleText'),
+                  style: TextStyle(color: Colors.white),
+                ),
+              );
+            },
           ),
+          /// Background color of the app bar.
           backgroundColor: Colors.teal,
+          actions: [
+            /// Action button to toggle search state.
+            Consumer<PaginationProvider>(
+              builder: (context, provider, child) {
+                return IconButton(
+                  /// Icon changes based on the search state.
+                  icon: Icon(
+                    provider.isSearching ? Icons.close : Icons.search,
+                    color: Colors.white,
+                  ),
+                  /// Toggles the search state and clears the search input if not searching.
+                  onPressed: () {
+                    provider.toggleSearch();
+                    if (!provider.isSearching) {
+                      provider.searchController.clear();
+                    }
+                  },
+                );
+              },
+            ),
+          ],
         ),
+        /// The body of the screen, which displays the list of items.
         body: Consumer<PaginationProvider>(
           builder: (context, provider, child) {
+            /// Determines which list of items to display based on the search state.
+            final items = provider.isSearching ? provider.resultSearch : provider.result;
             return provider.loading
                 ? ListView.builder(
-                    itemCount: 15,
-                    itemBuilder: (context, index) {
-                      return const ShimmerRow();
-                    },
-                  )
-                : DynamicSliverListView<Result>(
-                    scrollController: provider.scrollController,
-                    items: provider.result,
-                    isLoadingMore: provider.loadingMore,
-                    itemBuilder: (context, item) {
-                      return DynamicCard(
-                        name: item.name,
-                        url: item.url,
-                      );
-                    },
-                    onRefresh: provider.refresh,
-                  );
+              /// Displays a shimmer effect while loading.
+              itemCount: 15,
+              itemBuilder: (context, index) {
+                return const ShimmerRow();
+              },
+            )
+                : DynamicListView<CategoryDetails>(
+              /// Scroll controller for the list view.
+              scrollController: provider.scrollController,
+              /// List of items to display.
+              items: items,
+              /// Indicates if more items are being loaded.
+              isLoadingMore: provider.loadingMore,
+              /// Builds each item in the list.
+              itemBuilder: (context, item) {
+                return DynamicCard(
+                  /// Name of the item.
+                  name: item.name,
+                  /// URL of the item.
+                  url: item.area.toString(),
+                );
+              },
+              /// Callback for refreshing the list.
+              onRefresh: provider.refresh,
+            );
           },
         ),
       ),
